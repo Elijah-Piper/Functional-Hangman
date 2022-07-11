e9bb68a01b5886e9573649c58c723c205cd42917
Elijah Piper <35882341+Elijah-Piper@users.noreply.github.com>
2022-07-11 10:32:11 -0700

    Reimplemented original game logic methods along with associated unit testing

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -3,10 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Pattern;
 
@@ -14,6 +11,8 @@ public class Hangman {
     private String word;
     private int attempts; // Available guesses remaining; decremented until loss condition
     private HashSet<Character> guessedSet;
+    private String playerName;
+    private int score;
 
     // CONSTRUCTORS
     public Hangman() {
@@ -21,6 +20,8 @@ public class Hangman {
         word = selectWord(1, 20);
         attempts = 6;
         guessedSet = new HashSet<>();
+        playerName = userInput("What is your name?\n", Pattern.compile(".*"), "New lines not allowed.\n");
+        score = 0;
     }
 
     public Hangman(int minLength, int maxLength) {
@@ -28,6 +29,8 @@ public class Hangman {
         word = selectWord(minLength, maxLength);
         attempts = 6;
         guessedSet = new HashSet<>();
+        playerName = userInput("What is your name?\n", Pattern.compile(".*"), "New lines not allowed.\n");
+        score = 0;
     }
 
     // GETTERS AND SETTERS
@@ -55,6 +58,18 @@ public class Hangman {
     public HashSet<Character> getGuessedSet() {
         return new HashSet<Character>(guessedSet);
     }
+    public void setPlayerName(String name) {
+        this.playerName = name;
+    }
+    public String getPlayerName() {
+        return playerName;
+    }
+    public void setScore(int s) {
+        this.score = s;
+    }
+    public int getScore() {
+        return score;
+    }
 
     public static String selectWord(int minLength, int maxLength) {
         if (maxLength < minLength) return "";
@@ -71,6 +86,78 @@ public class Hangman {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return "";
+    }
+
+    public String generateGallows() {
+        // Reads gallows ascii art from 1 of 7 'gallow' text files, returning properly formatted string
+        String gallows = "";
+        try {
+            gallows = Files.readAllLines(Paths.get("src/main/resources/gallow%d.txt".formatted(attempts))).stream()
+                    .reduce("", (acc, line) -> acc.concat(line + "\n"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return gallows;
+    }
+
+    public String generateMissed() {
+        String output = "Guessed letters: ";
+        for (char c: guessedSet.stream().sorted().toList()) {
+            output = output.length() == 17 ? output.concat("" + c) : output.concat(", " + c);
+        }
+        return output;
+    }
+
+    public String generateHiddenWord() {
+        String hiddenWord = "";
+        for (char c: word.toCharArray()) {
+            if (guessedSet.contains(c)) {
+                hiddenWord = hiddenWord.concat("" + c);
+            } else {
+                hiddenWord = hiddenWord.concat("_");
+            }
+        }
+        return hiddenWord;
+    }
+
+    public static boolean userInputYesOrNo(String question) {
+        // Asks the user a yes or no question, returning boolean depending on answer, using recursion for input validation
+        Scanner sc = new Scanner(System.in);
+
+        String suffix = "(y or n)\n";
+        System.out.println(question + suffix);
+        try {
+            String response = sc.nextLine().toLowerCase();
+            if (response.equals("y")) return true;
+            else if (response.equals("n")) return false;
+            else {
+                System.out.println("Invalid input. Please enter " + suffix);
+                return userInputYesOrNo(question);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // This statement should never be reached
+        return false;
+    }
+
+    public static String userInput(String question, Pattern validInputs, String invalidInputSuffix) {
+        // Asks user a question using recursion for input validation
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(question);
+        try {
+            String response = sc.nextLine().toLowerCase().strip();
+            if (validInputs.matcher(response).matches()) return response;
+            else {
+                System.out.println("Invalid input. Please enter " + invalidInputSuffix);
+                userInput(question, validInputs, invalidInputSuffix);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // This statement should never be reached
         return "";
     }
 
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayInputStream;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,5 +22,55 @@ class HangmanTest {
     void selectWord_ReturnsWordForValidSizes(int minValue, int maxValue) {
         // Assuming there is a valid word found, a string of length greater than 0 should be returned
         assertTrue(Hangman.selectWord(minValue, maxValue).length() > 0);
+    }
+
+    // Hangman instance.generateMissed tests
+    @Test
+    void generateMissedReturnsOutputSorted() {
+        Hangman game = new Hangman();
+        game.addToGuessedSet('d');
+        game.addToGuessedSet('c');
+        game.addToGuessedSet('b');
+        game.addToGuessedSet('a');
+        assert(game.generateMissed().equals("Guessed letters: a, b, c, d"));
+    }
+    @Test
+    void generateMissedIsCaseInsensitive() {
+        Hangman game = new Hangman();
+        game.addToGuessedSet('A');
+        game.addToGuessedSet('a');
+        assert (game.generateMissed().equals("Guessed letters: a"));
+    }
+
+    // Hangman.useInput tests
+    @Test
+    void userInputForDigitsReturnsDigits() {
+        System.setIn(new ByteArrayInputStream("3".getBytes()));
+        String output = Hangman.userInput(
+                "",
+                Pattern.compile("[\\d]"),
+                ""
+        );
+        assert(output.equals("3"));
+    }
+    @Test
+    void userInputForCharsReturnsChars() {
+        System.setIn(new ByteArrayInputStream("d".getBytes()));
+        String output = Hangman.userInput(
+                "",
+                Pattern.compile("[A-Za-z]"),
+                ""
+        );
+        assert(output.equals("d"));
+    }
+
+    // Hangman instance.generateHiddenWord tests
+    @Test
+    void generateHiddenWordAddsCorrectGuesses() {
+        System.setIn(new ByteArrayInputStream("name".getBytes()));
+        Hangman game = new Hangman();
+        game.setWord("apple");
+        game.addToGuessedSet('p');
+        assert(game.generateHiddenWord().equals("_pp__"));
     }
 }
